5190983c4dc92479b7aebffc9ac0c8451625cc01
misterjeckyll <pantryw@gmail.com>
2017-05-15 18:12:40 +0200

    gui modification, get width and depth from selection

--- fablab_box_selection_generator.py
+++ fablab_box_selection_generator.py
@@ -1,3 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Inkscape Extension to generate an svg box pattern to be cut with a laser cutter
+'''
+
 import sys
 sys.path.append('/usr/share/inkscape/extensions')
 
@@ -35,55 +41,53 @@ class BoxSelectionGeneratorEffect(BaseEffect, BoxEffect):
         # Call the base class constructor.
         BaseEffect.__init__(self)
 
-        self.OptionParser.add_option('-i', '--path_id', action='store',
-                                     type='string', dest='path_id', default='box',
-                                     help='Id of svg path')
-
-        self.OptionParser.add_option('--height', action='store',
-                                     type='float', dest='height', default=50,
-                                     help='Hauteur de la boite')
-        self.OptionParser.add_option('--thickness', action='store',
-                                     type='float', dest='thickness', default=3,
-                                     help='Epaisseur du materiau')
-
-        self.OptionParser.add_option('--backlash', action='store',
-                                     type='float', dest='backlash', default=0.1,
-                                     help='Matiere enleve par le laser')
-        self.OptionParser.add_option('--top', action="store",
-                                     type='string', dest='top', help='top edge', default='e')
-
+        self.OptionParser.add_option('-i', '--path_id', action='store',type='string',   dest='path_id',     default='box',  help='Id of svg path')
+        self.OptionParser.add_option('--height',        action='store',type='float',    dest='height',      default=50,     help='Hauteur de la boite')
+        self.OptionParser.add_option('--thickness',     action='store',type='float',    dest='thickness',   default=3,      help='Epaisseur du materiau')
+        self.OptionParser.add_option('--backlash',      action='store',type='float',    dest='backlash',    default=0.1,    help='Matière enlevé par le laser')
+        self.OptionParser.add_option('--top',           action="store",type='string',   dest='top',         default='e',    help='top edge')
+        self.OptionParser.add_option('--tab_size',      action='store',type='float',    dest='tab_size',    default=10,     help='Tab size')
+        self.OptionParser.add_option("", "--active-tab",action="store",type="string",   dest="active_tab",  default='title',help="Active tab.")
         self.start_stop = {}
 
-# ----------------------------------------------------------------------#
+# ------------------------------------------------------------------#
 ### Main function called when the extension is run.
-# ----------------------------------------------------------------------#
+# ------------------------------------------------------------------#
     def effect(self):
         """ Calculate Box cutting path from selection and options
         """
         ### Global params - Debug
-
         parent = self.current_layer
         centre = self.view_center
         fgcolor = "#FF0000"
         bgcolor = None
         choices = ['e', 'c', 'E', 'S', 'i', 'k', 'v', 'f', 'L']
+        width, depth = 0,0
 
         inkex.debug("-- %s -- %s --" % (centre[0], centre[1]))
         inkex.debug(self.options)
 
         ### Gather incoming params from options and selection
 
+        for id,node in self.selected.iteritems():
+            if node.tag == inkex.addNS('rect','svg'):
+                width = node.get('height')
+                depth = node.get('width')
+                inkex.debug("largeur: %s - profondeur: %s"%(width,depth))
+            else:
+                inkex.debug(node)
         height = self.options.height
+        if(width==0 and depth == 0):
+            inkex.debug("Aucun rectangle trouvé dans la selection")
 
         if(True):
-            for shape in self.box_with_top(self.options.path_id, centre[0], centre[1], bgcolor, fgcolor, self.options.width, self.options.depth, self.options.height, self.options.tab_size, self.options.thickness, self.options.backlash,self.options.backlash):
+            for shape in self.box_with_top(self.options.path_id, centre[0], centre[1], bgcolor, fgcolor, width, depth, height, self.options.tab_size, self.options.thickness, self.options.backlash):
                 inkex.etree.SubElement(parent, inkex.addNS('path', 'svg'), shape)
         else:
-            for shape in self.box_without_top(self.options.path_id, centre[0], centre[1], bgcolor, fgcolor, self.options.width, self.options.depth, self.options.height, self.options.tab_size, self.options.thickness, self.options.backlash):
+            for shape in self.box_without_top(self.options.path_id, centre[0], centre[1], bgcolor, fgcolor, width, depth, height, self.options.tab_size, self.options.thickness, self.options.backlash):
                 inkex.etree.SubElement(parent, inkex.addNS('path', 'svg'), shape)
 
         #inkex.etree.SubElement(parent, inkex.addNS('path','svg'), ell_attribs )
-
 
 if __name__ == '__main__':
     effect = BoxSelectionGeneratorEffect()
